b9f8e245cde21eafb67a6142b03d9dc10acb1e60
spandey <spandey128@gmail.com>
2017-08-08 20:38:03 +0530

    view pagers

--- app/src/main/java/app/techinshorts/techinshortsapp/MainActivity.java
+++ app/src/main/java/app/techinshorts/techinshortsapp/MainActivity.java
@@ -3,70 +3,107 @@ package app.techinshorts.techinshortsapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.android.volley.*;
 import com.android.volley.toolbox.*;
+import com.emoiluj.doubleviewpager.DoubleViewPager;
+import com.emoiluj.doubleviewpager.DoubleViewPagerAdapter;
 import com.squareup.picasso.Picasso;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-  @Override
+    private DoubleViewPager viewpager;
+    private int horizontalChilds;
+    private int verticalChilds;
+
+
+    @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-    RequestQueue mRequestQueue;
-    Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into((ImageView) findViewById(R.id.image));
-    final TextView textView = (TextView) findViewById(R.id.textview);
-// Instantiate the cache
-    Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
-
-// Set up the network to use HttpURLConnection as the HTTP client.
-    Network network = new BasicNetwork(new HurlStack());
-
-// Instantiate the RequestQueue with the cache and network.
-    mRequestQueue = new RequestQueue(cache, network);
-
-// Start the queue
-    mRequestQueue.start();
-
-    JsonArrayRequest jsObjRequest = new JsonArrayRequest
-        (Request.Method.GET, "http://192.168.0.108:3000/news.json", null, new Response.Listener<JSONArray>() {
-
-          @Override
-          public void onResponse(JSONArray response) {
-            System.out.println("Response: "  + response);
-            textView.setText(response.toString());
-          }
-        }, new Response.ErrorListener() {
-
-          @Override
-          public void onErrorResponse(VolleyError error) {
-            System.out.println("VolleyError " + error);
-            // TODO Auto-generated method stub
-
-          }
-        });
-    jsObjRequest.setShouldCache(true);
-    mRequestQueue.add(jsObjRequest);
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
+//    super.onCreate(savedInstanceState);
+//    setContentView(R.layout.activity_main);
+//    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//    setSupportActionBar(toolbar);
+//    RequestQueue mRequestQueue;
+//    Picasso.with(this).load("http://i.imgur.com/DvpvklR.png").into((ImageView) findViewById(R.id.image));
+//    final TextView textView = (TextView) findViewById(R.id.textview);
+//// Instantiate the cache
+//    Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+//
+//// Set up the network to use HttpURLConnection as the HTTP client.
+//    Network network = new BasicNetwork(new HurlStack());
+//
+//// Instantiate the RequestQueue with the cache and network.
+//    mRequestQueue = new RequestQueue(cache, network);
+//
+//// Start the queue
+//    mRequestQueue.start();
+//
+//    JsonArrayRequest jsObjRequest = new JsonArrayRequest
+//        (Request.Method.GET, "http://192.168.0.108:3000/news.json", null, new Response.Listener<JSONArray>() {
+//
+//          @Override
+//          public void onResponse(JSONArray response) {
+//            System.out.println("Response: "  + response);
+//            textView.setText(response.toString());
+//          }
+//        }, new Response.ErrorListener() {
+//
+//          @Override
+//          public void onErrorResponse(VolleyError error) {
+//            System.out.println("VolleyError " + error);
+//            // TODO Auto-generated method stub
+//
+//          }
+//        });
+//    jsObjRequest.setShouldCache(true);
+//    mRequestQueue.add(jsObjRequest);
+//    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//    fab.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View view) {
+//        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//            .setAction("Action", null).show();
+//      }
+//    });
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
+        horizontalChilds = 3;
+        verticalChilds = 3;
+        loadUI();
   }
+
+    private void loadUI() {
+
+        ArrayList<PagerAdapter> verticalAdapters = new ArrayList<PagerAdapter>();
+        generateVerticalAdapters(verticalAdapters);
+
+        viewpager = (DoubleViewPager) findViewById(R.id.pager);
+        viewpager.setAdapter(new DoubleViewPagerAdapter(getApplicationContext(), verticalAdapters));
+        viewpager.setCurrentItem(1);
+    }
+
+    private void generateVerticalAdapters(ArrayList<PagerAdapter> verticalAdapters) {
+        for (int i=0; i<horizontalChilds; i++){
+            int c = i == 1 ? 5 : 1;
+            verticalAdapters.add(new VerticalPagerAdapter(this, i, c));
+        }
+    }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/app/techinshorts/techinshortsapp/VerticalPagerAdapter.java
@@ -0,0 +1,95 @@
+package app.techinshorts.techinshortsapp;
+
+
+        import android.content.Context;
+        import android.graphics.Color;
+        import android.support.v4.view.PagerAdapter;
+        import android.util.Log;
+        import android.view.Gravity;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.LinearLayout;
+        import android.widget.RelativeLayout.LayoutParams;
+        import android.widget.TextView;
+
+        import org.json.JSONArray;
+        import org.json.JSONException;
+
+        import java.io.IOException;
+        import java.io.InputStream;
+
+public class VerticalPagerAdapter extends PagerAdapter{
+
+    private Context mContext;
+    private int mParent;
+    private int mChilds;
+    private JSONArray mColors;
+
+    public VerticalPagerAdapter(Context c, int parent, int childs){
+        mContext = c;
+        mParent = parent;
+        mChilds = childs;
+        loadJSONFromAsset(c);
+    }
+
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    @Override
+    public int getCount() {
+        return mChilds;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        LinearLayout linear = new LinearLayout(mContext);
+        linear.setOrientation(LinearLayout.VERTICAL);
+        linear.setGravity(Gravity.CENTER);
+        LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        linear.setLayoutParams(lp);
+
+        TextView tvParent = new TextView(mContext);
+        tvParent.setGravity(Gravity.CENTER_HORIZONTAL);
+        tvParent.setText("Parent:" + mParent);
+        tvParent.setTextColor(Color.BLACK);
+        tvParent.setTextSize(70);
+        linear.addView(tvParent);
+
+        TextView tvChild = new TextView(mContext);
+        tvChild.setGravity(Gravity.CENTER_HORIZONTAL);
+        tvChild.setText("Child:" + position);
+        tvChild.setTextColor(Color.BLACK);
+        tvChild.setTextSize(70);
+        linear.addView(tvChild);
+
+        setColors(position, linear);
+        container.addView(linear);
+        return linear;
+    }
+
+    public void setColors(int position, View layout){
+
+        try {
+            String colorString = "#FFF";
+            layout.setBackgroundColor(Color.parseColor(colorString));
+        } catch (Exception ex){
+            Log.e("XXX", "Fail to load color ["+mParent+"]["+position+"]");
+        }
+
+    }
+
+    public void loadJSONFromAsset(Context ctx) {
+    }
+}
