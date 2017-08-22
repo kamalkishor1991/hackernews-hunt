cf7e48ec96e31385272fb8b9b91dab64e1c587dd
kamal <kkjoshi.it@gmail.com>
2017-08-22 19:59:39 +0530

    splash activity and basic gcm setup

---
+++ app/src/main/java/app/techinshorts/techinshortsapp/GCMPushReceiverService.java
@@ -0,0 +1,48 @@
+package app.techinshorts.techinshortsapp;
+
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.android.gms.gcm.GcmListenerService;
+
+
+/**
+ * Created by Belal on 4/15/2016.
+ */
+
+//Class is extending GcmListenerService
+public class GCMPushReceiverService extends GcmListenerService {
+
+    //This method will be called on every new message received
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+        //Getting the message from the bundle
+        String message = data.getString("message");
+        //Displaying a notiffication with the message
+        sendNotification(message);
+    }
+
+    //This method is generating a notification and displaying the notification
+    private void sendNotification(String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        int requestCode = 0;
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, requestCode, intent, PendingIntent.FLAG_ONE_SHOT);
+        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder noBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, noBuilder.build()); //0 = ID of notification
+    }
+}
---
+++ app/src/main/java/app/techinshorts/techinshortsapp/GCMRegistrationIntentService.java
@@ -0,0 +1,64 @@
+package app.techinshorts.techinshortsapp;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+
+/**
+ * Created by Belal on 4/15/2016.
+ */
+public class GCMRegistrationIntentService extends IntentService {
+    //Constants for success and errors
+    public static final String REGISTRATION_SUCCESS = "RegistrationSuccess";
+    public static final String REGISTRATION_ERROR = "RegistrationError";
+
+    //Class constructor
+    public GCMRegistrationIntentService() {
+        super("");
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        //Registering gcm to the device
+        registerGCM();
+    }
+
+    private void registerGCM() {
+        //Registration complete intent initially null
+        Intent registrationComplete = null;
+
+        //Register token is also null
+        //we will get the token on successfull registration
+        String token = null;
+        try {
+            //Creating an instanceid
+            InstanceID instanceID = InstanceID.getInstance(getApplicationContext());
+
+            //Getting the token from the instance id
+            token = instanceID.getToken(getString(R.string.gcm_defaultSenderId), GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+
+            //Displaying the token in the log so that we can copy it to send push notification
+            //You can also extend the app by storing the token in to your server
+            Log.w("GCMRegIntentService", "token:" + token);
+
+            //on registration complete creating intent with success
+            registrationComplete = new Intent(REGISTRATION_SUCCESS);
+
+            //Putting the token to the intent
+            registrationComplete.putExtra("token", token);
+        } catch (Exception e) {
+            //If any error occurred
+            Log.w("GCMRegIntentService", "Registration error");
+            registrationComplete = new Intent(REGISTRATION_ERROR);
+        }
+
+        //Sending the broadcast that registration is completed
+        LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
+    }
+}
