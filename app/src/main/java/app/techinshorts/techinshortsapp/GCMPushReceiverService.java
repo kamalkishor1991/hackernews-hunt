e0ad6ebd382ccc6d5fbc993ce58c8761e23d2335
kamal <kkjoshi.it@gmail.com>
2017-09-02 13:33:53 +0530

    custom notification(not complete)

--- app/src/main/java/app/techinshorts/techinshortsapp/GCMPushReceiverService.java
+++ app/src/main/java/app/techinshorts/techinshortsapp/GCMPushReceiverService.java
@@ -7,16 +7,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.RingtoneManager;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
+import android.widget.RemoteViews;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.google.android.gms.gcm.GcmListenerService;
 import com.google.firebase.crash.FirebaseCrash;
-import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 
@@ -62,23 +61,36 @@ public class GCMPushReceiverService extends GcmListenerService {
             }
         });
     }
-    //This method is generating a notification and displaying the notification
+    //This method is generating a notification_small and displaying the notification_small
     private void sendNotification(String message, String summary, String imageURL) {
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         int requestCode = 0;
         PendingIntent pendingIntent = PendingIntent.getActivity(this, requestCode, intent, PendingIntent.FLAG_ONE_SHOT);
-        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+       // Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         Bitmap remote_picture = null;
-
+       // NotificationCompat.
 // Create the style object with BigPictureStyle subclass.
         NotificationCompat.BigPictureStyle notiStyle = new
                 NotificationCompat.BigPictureStyle();
         notiStyle.setBigContentTitle(message);
-        notiStyle.setSummaryText(summary);
+        notiStyle.setSummaryText(message);
+        //notiStyle.setSummaryText(summary);
 
+       /* NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+        bigTextStyle.setBigContentTitle(message);
+        bigTextStyle.bigText(summary);*/
+        RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.notification_small);
+        contentView.setImageViewResource(R.id.image, R.mipmap.ic_launcher);
+        contentView.setTextViewText(R.id.title, "Custom notification_small");
+        contentView.setTextViewText(R.id.text, "This is a custom layout");
 
+        RemoteViews contentViewBig = new RemoteViews(getPackageName(), R.layout.notification_big);
+        contentViewBig.setImageViewResource(R.id.image, R.mipmap.ic_launcher);
+        contentViewBig.setTextViewText(R.id.title, "Custom notification_big");
+        contentViewBig.setTextViewText(R.id.text, "This is a custom layout...................");
+        contentViewBig.setTextColor(R.id.text, Color.BLACK);
         try {
             remote_picture = BitmapFactory.decodeStream(
                     (InputStream) new URL(imageURL).getContent());
@@ -86,17 +98,18 @@ public class GCMPushReceiverService extends GcmListenerService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        contentViewBig.setImageViewBitmap(R.id.big_picture, remote_picture);
+        //contentViewBig.setTextViewText(R.id.big_text, "kdasfjlk asdflkjdsa jflkdsajflkj dsafjdsajflksajd;lkfjsad;jflkdsajflk;saj;lkfjsad fjdsajf;lkdsaj;lkfjsafj;ldsajf;lkdsajf;lksajd;fj");
+
         NotificationCompat.Builder noBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setLargeIcon(remote_picture)
-                .setContentText(message)
-                .setSound(sound)
-                .setAutoCancel(true).setStyle(notiStyle)
+                .setContent(contentView)
+                .setCustomBigContentView(contentViewBig)
                 .setContentIntent(pendingIntent);
 // Add the big picture to the style.
         notiStyle.bigPicture(remote_picture);
 
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(new Random().nextInt(), noBuilder.build()); //0 = ID of notification
+        notificationManager.notify(new Random().nextInt(), noBuilder.build()); //0 = ID of notification_small
     }
 }
