6bea61371674292a5e7d85f9c5ff361a1f1b17ed
kamal <kkjoshi.it@gmail.com>
2017-09-02 23:09:51 +0530

    first version upload

--- app/src/main/java/com/hnhunt/hnhunt/utils/Utility.java
+++ app/src/main/java/com/hnhunt/hnhunt/utils/Utility.java
@@ -24,6 +24,7 @@ import java.util.Date;
 
 public class Utility {
     public static final String BASE_URL = "http://hnhunt.com";
+
     public static String fetchApi(String... args) {
         String url = BASE_URL + "/news.json";
         StringBuilder params = new StringBuilder();
